b3d483d6ab55d0e336c5a2b433506161730f54df
katreenapo <katja.djadechko1@gmail.com>
2021-02-22 19:40:07 +0200

    Lesson-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  name = "Katyushka";
 
+  number = 10;
+
+  arrNumber = [10, 40, 60, 50];
+
+  products = [{
+    name: 'Product 1',
+    price: 15,
+    isStock: true
+  },{
+    name: 'Product 2',
+    price: 10,
+    isStock: true
+  },{
+    name: 'Product 3',
+    price: 35,
+    isStock: false
+  },{
+    name: 'Product 4',
+    price: 85,
+    isStock: true
+  }];
+
+  isOpenMenu = false;
+
+  openDropdown(){
+    this.isOpenMenu = !this.isOpenMenu;
+  }
+
+  addProduct() {
+    this.products.push({
+      name: 'Product 5',
+      price: 35,
+      isStock: true
+    });
+
+   
+  }
 }
